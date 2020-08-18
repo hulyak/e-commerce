31880bdf732bc140e3ac25601ad16dbc8ea3f928
hulyak <57311842+hulya95@users.noreply.github.com>
2020-08-18 08:02:42 -0700

    save records to json

--- repositories/users.js
+++ repositories/users.js
@@ -2,23 +2,42 @@ const fs = require('fs')
 
 class UsersRepository {
   //check if the user info is saved into a json file
-  constructor(filename) {  
+  constructor(filename) {
     if (!filename) {
       throw new Error('Creating a repository requires a filename')
     }
 
     this.filename = filename;
 
-    //check if the file exists, if it doesn't exist create file
+    //check if the file exists, if it doesn't exist, create new file
     //constructor function cannot be async 
     try {
       fs.accessSync(this.filename);
     } catch (err) {
       fs.writeFileSync(this.filename, '[]')
     }
-    
+  }
 
+  async getAll() {
+    //open the file called this.filename, parse the contents of json data
+    return JSON.parse(await fs.promises.readFile(this.filename, {
+      encoding: 'utf-8'
+    }));
+  }
+
+  async create(attributes) {
+    const records = await this.getAll();
+    records.push(attributes)  
+    //write the updated 'records' array back to this.filename
+   await fs.promises.writeFile(this.filename, JSON.stringify(records))
   }
 }
 
-new UsersRepository('users.json');
+  const test = async () => {
+    const repo = new UsersRepository('users.json');
+    await repo.create({email : 'test@test.com', password : 'password'})
+    const users = await repo.getAll();
+
+    console.log(users)
+  };
+  test();
