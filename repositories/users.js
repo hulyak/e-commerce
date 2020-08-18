592b09e8904eb738a5f32714791c41b6614bec85
hulyak <57311842+hulya95@users.noreply.github.com>
2020-08-18 08:33:04 -0700

    find the users by id

--- repositories/users.js
+++ repositories/users.js
@@ -46,13 +46,19 @@ class UsersRepository {
   randomId () {
     return crypto.randomBytes (4).toString ('hex'); //turn buffer data into hex
   }
+
+  async getOne (id) {
+    const records = await this.getAll ();
+    return records.find (record => record.id === id);
+  }
 }
 
 const test = async () => {
   const repo = new UsersRepository ('users.json');
-  await repo.create ({email: 'test@test.com', password: 'password'});
-  const users = await repo.getAll ();
-  console.log (users);
+  // await repo.create ({email: 'test@test.com', password: 'password'});
+  // const users = await repo.getAll ();
+  const user = await repo.getOne ('4b434f0e');
+  console.log (user);
 };
 
 test ();
