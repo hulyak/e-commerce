450650b4eaf3b133c3b27e1b9da13e8c706b56b8
hulyak <57311842+hulya95@users.noreply.github.com>
2020-08-18 08:08:02 -0700

    move create new user functionality to writeAll and change the formatting

--- repositories/users.js
+++ repositories/users.js
@@ -1,43 +1,51 @@
-const fs = require('fs')
+const fs = require ('fs');
 
 class UsersRepository {
   //check if the user info is saved into a json file
-  constructor(filename) {
+  constructor (filename) {
     if (!filename) {
-      throw new Error('Creating a repository requires a filename')
+      throw new Error ('Creating a repository requires a filename');
     }
 
     this.filename = filename;
 
     //check if the file exists, if it doesn't exist, create new file
-    //constructor function cannot be async 
+    //constructor function cannot be async
     try {
-      fs.accessSync(this.filename);
+      fs.accessSync (this.filename);
     } catch (err) {
-      fs.writeFileSync(this.filename, '[]')
+      fs.writeFileSync (this.filename, '[]');
     }
   }
 
-  async getAll() {
+  async getAll () {
     //open the file called this.filename, parse the contents of json data
-    return JSON.parse(await fs.promises.readFile(this.filename, {
-      encoding: 'utf-8'
-    }));
+    return JSON.parse (
+      await fs.promises.readFile (this.filename, {
+        encoding: 'utf-8',
+      })
+    );
   }
 
-  async create(attributes) {
-    const records = await this.getAll();
-    records.push(attributes)  
+  async create (attributes) {
+    const records = await this.getAll ();
+    records.push (attributes);
     //write the updated 'records' array back to this.filename
-   await fs.promises.writeFile(this.filename, JSON.stringify(records))
+    await this.writeAll (records);
+  }
+
+  async writeAll (records) {
+    await fs.promises.writeFile (
+      this.filename,
+      JSON.stringify (records, null, 2) //2 level of indentation
+    );
   }
 }
 
-  const test = async () => {
-    const repo = new UsersRepository('users.json');
-    await repo.create({email : 'test@test.com', password : 'password'})
-    const users = await repo.getAll();
-
-    console.log(users)
-  };
-  test();
+const test = async () => {
+  const repo = new UsersRepository ('users.json');
+  await repo.create ({email: 'test@test.com', password: 'password'});
+  const users = await repo.getAll ();
+  console.log (users);
+};
+test ();
