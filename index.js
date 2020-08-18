d2b07d95592e279ed480903efe9c03f806d41f91
hulyak <57311842+hulya95@users.noreply.github.com>
2020-08-18 15:43:03 -0700

    sign out the user

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cookieSession({
   keys: ['fnsdfsdndnnnmsfnsmnfs'] //encrypt information
 }));
 
-app.get ('/', (req, res) => {
+app.get ('/signup', (req, res) => {
   res.send (`
   <div>
   Your id is: ${req.session.userId}
@@ -24,7 +24,7 @@ app.get ('/', (req, res) => {
   `);
 });
 
-app.post ('/', async (req, res) => {
+app.post ('/signup', async (req, res) => {
   const {email, password, passwordConfirmation} = req.body;
 
   const existingUser = await usersRepo.getOneBy ({email: email});
@@ -44,6 +44,27 @@ app.post ('/', async (req, res) => {
   req.session.userId = user.id;
   res.send ('account created');
 });
+
+app.get('/signout', (req, res) => {
+  req.session = null; //destroy the session cookies
+  res.send('You are logged out')
+})
+
+app.get('/signin', (req, res) => {
+  res.send(`
+  <div>
+    <form method="POST">
+      <input name="email" placeholder="email"/>
+      <input name="password" placeholder="password" />
+      <button>Sign In</button>
+    </form>
+  </div>
+  `)
+})
+
+app.post('/signin', async(req, res) => {
+
+})
 
 app.listen (3000, () => {
   console.log ('listening on port 3000');
