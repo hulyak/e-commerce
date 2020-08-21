fcf60de558079e660dfa427fdcd7969cba0a0f6a
hulyak <57311842+hulya95@users.noreply.github.com>
2020-08-21 10:55:09 -0700

    adding items to cart

--- routes/carts.js
+++ routes/carts.js
@@ -16,9 +16,22 @@ router.post('/cart/products', async (req, res) => {
     //we have a cart! Lets get it from the repo
     cart = await cartsRepo.getOne(req.session.cartId);
   }
-  console.log(cart);
+
   //Either increment quantity for existing product
   //Or add new product to items array
+  const existingItem = cart.items.find(
+    (item) => (item.id = req.body.productId)
+  );
+  if (existingItem) {
+    existingItem.quantity++;
+  } else {
+    cart.items.push({ id: req.body.productId, quantity: 1 });
+  }
+
+  await cartsRepo.update(cart.id, {
+    items: cart.items,
+  });
+
   res.send('Product added to cart');
 });
 
