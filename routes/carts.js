f5a14011e7b3ec0ec0a848b6d92de7e146860071
hulyak <57311842+hulya95@users.noreply.github.com>
2020-08-21 10:00:54 -0700

    shopping cart boilerplate

--- routes/carts.js
+++ routes/carts.js
@@ -3,6 +3,11 @@ const express = require('express');
 const router = express.Router();
 
 //Receive a POST request to add an item to a cart
+router.post('/cart/products', (req, res) => {
+  console.log(req.body.productId);
+
+  res.send('Product added to cart');
+});
 
 //Receive a GET request to show all items in cart
 
